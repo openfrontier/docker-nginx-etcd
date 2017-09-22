3cb293dd08ad9a57c95317e8613f4b18a69746cd
thinkernel <thinkernel@gmail.com>
2017-09-22 16:47:05 +0800

    echo config file before startup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 set -e
 
 confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
+cat /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
