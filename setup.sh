16d328f07050ecb79232ad2c29560e345ee035e3
邢 久东 <xing.jiudong@trans-cosmos.com.cn>
2018-02-12 11:54:43 +0800

    get certificate from etcd

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+# Get certificate
+etcdctl get /nginx-config/ssl/PROXY_SITE_URL/chain.pem > /etc/nginx/conf.d/chain.crt
+etcdctl get /nginx-config/ssl/PROXY_SITE_URL/private.key > /etc/nginx/conf.d/private.key
+
 confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
 cat /etc/nginx/conf.d/default.conf
 
