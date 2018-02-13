68b2313b1a5c13fbc954472e0a371d887ef8ea51
邢 久东 <xing.jiudong@trans-cosmos.com.cn>
2018-02-13 18:54:10 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Get certificate
-count=$(etcdctl ls /nginx-config/internet-ssl-certificate/url | wc -l)
+count=$(etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 ls /nginx-config/internet-ssl-certificate/url | wc -l)
 
 for((i=1;i<=${count} ;i++));
-do urls=`etcdctl get /nginx-config/internet-ssl-certificate/url/$i`;
-   etcdctl get /nginx-config/internet-ssl-certificate/${urls}.pem > /${urls}.pem;
-   etcdctl get /nginx-config/internet-ssl-certificate/${urls}.key > /${urls}.key;
+do urls=`etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/url/$i`;
+   etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/${urls}.pem > /${urls}.pem;
+   etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/${urls}.key > /${urls}.key;
    chmod 600 /${urls}.pem;
    chmod 600 /${urls}.key;
 done
