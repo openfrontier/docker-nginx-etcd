4f2dc082d95f8b362f837cb0ca26b39e6aa4076c
邢 久东 <xing.jiudong@trans-cosmos.com.cn>
2018-02-13 18:51:56 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,16 @@
 set -e
 
 # Get certificate
-etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/ssl/${PROXY_SITE_URL}/chain.pem > /etc/nginx/conf.d/chain.pem
-etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/ssl/${PROXY_SITE_URL}/private.key > /etc/nginx/conf.d/private.key
+count=$(etcdctl ls /nginx-config/internet-ssl-certificate/url | wc -l)
+
+for((i=1;i<=${count} ;i++));
+do urls=`etcdctl get /nginx-config/internet-ssl-certificate/url/$i`;
+   etcdctl get /nginx-config/internet-ssl-certificate/${urls}.pem > /${urls}.pem;
+   etcdctl get /nginx-config/internet-ssl-certificate/${urls}.key > /${urls}.key;
+   chmod 600 /${urls}.pem;
+   chmod 600 /${urls}.key;
+done
+
 
 confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
 cat /etc/nginx/conf.d/default.conf
