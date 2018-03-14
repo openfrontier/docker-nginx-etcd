6d0e91fe682afbeea6f10f18a5337e831dc00b22
邢 久东 <xing.jiudong@trans-cosmos.com.cn>
2018-03-14 16:30:48 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,22 @@
 set -e
 
 # Get certificate
-count=$(etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 ls /nginx-config/internet-ssl-certificate/url | wc -l)
+count=$(etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 ls /nginx-config/${PROJECT_NAME}-certificate/url > /dev/null 2>&1 | wc -l)
 
-for((i=1;i<=${count} ;i++));
-do urls=`etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/url/$i`;
-   etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/${urls}.pem > /${urls}.pem;
-   etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/internet-ssl-certificate/${urls}.key > /${urls}.key;
-   chmod 600 /${urls}.pem;
-   chmod 600 /${urls}.key;
-done
+if [ ${count} -eq 0 ]
+then
+  confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
+else
+  for((i=1;i<=${count} ;i++));
+  do urls=`etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/${PROJECT_NAME}-certificate/url/$i`;
+     etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/${PROJECT_NAME}-certificate/${urls}.pem > /${urls}.pem;
+     etcdctl --endpoints http://${ETCD_CLIENT_IP}:2379 get /nginx-config/${PROJECT_NAME}-certificate/${urls}.key > /${urls}.key;
+     chmod 600 /${urls}.pem;
+     chmod 600 /${urls}.key;
+  done
+  confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
+fi
 
-
-confd -onetime -backend etcd -node http://${ETCD_CLIENT_IP}:2379 --prefix="/nginx-config/${PROJECT_NAME}"
 cat /etc/nginx/conf.d/default.conf
 
 exec nginx -g 'daemon off;'
